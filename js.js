3808bd23e22edc47e05f124c47abcbf379a50669
katehc <129056549+katehc@users.noreply.github.com>
2023-07-19 00:05:08 +0100

    update menu

--- js.js
+++ js.js
@@ -97,7 +97,7 @@ function setDate(val) {
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-    var x = document.getElementById("menuBar");
+    var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
