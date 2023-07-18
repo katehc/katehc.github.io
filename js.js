f6abf7a56ecdbef76bae4a79ab3664fa24201e19
katehc <129056549+katehc@users.noreply.github.com>
2023-07-18 23:49:18 +0100

    Update js.js

--- js.js
+++ js.js
@@ -94,3 +94,13 @@ var date;
 function setDate(val) {
     date = val;
 }
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("menuBar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
