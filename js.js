e7a8681eef2de47ac53ec7a254a5b48a858e8098
katehc <129056549+katehc@users.noreply.github.com>
2023-07-19 08:25:02 +0100

    menu test

--- js.js
+++ js.js
@@ -91,11 +91,20 @@ function setDate(val) {
 }
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function myFunction2() {
   var x = document.getElementById("menuBar");
   if (x.className === "menuBar") {
     x.className += " responsive";
   } else {
     x.className = "menuBar";
+  }
+}
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
